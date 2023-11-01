7773c1796baf9b8782680599d729461faa01a5a0
JWei9 <JWei9@slb.com>
2023-11-01 14:42:16 +0800

    test: add test for normal class number

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -6,12 +6,15 @@ namespace FizzBuzzTest
     public class FizzBuzzGameTest
     {
         [Fact]
-        public void Test1()
+        public void Should_return_the_number_given_when_countOff_given_a_normal_case_number()
         {
-            FizzBuzzGame game = new FizzBuzzGame();
+            //given
+            int givenNumber = 1;
+            //when
+            string result = FizzBuzzGame.CountOff(givenNumber);
 
             //then
-            Assert.NotNull(game);
+            Assert.Equal("1", result);
         }
     }
 }
